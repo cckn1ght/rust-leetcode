8a85017c8dade7bdcb92ba22fb5ef7d7d7cf1ba7
qunhao <qunhao.song@transwarp.io>
2021-03-26 19:00:46 +0800

    refactor

--- src/commands.rs
+++ src/commands.rs
@@ -5,8 +5,8 @@ pub fn run_setup_command(matches: &ArgMatches) {
     let path = create_project_folder(matches).unwrap_or_else(handle_error);
     create_templates(path).unwrap_or_else(handle_error);
 }
-pub fn run_random_command(matches: &ArgMatches) {
 
+pub fn run_random_command(matches: &ArgMatches) {
     let difficulty = matches.value_of("difficulty");
     let difficulty_num: Option<u32> = difficulty.map(|d| match d.to_lowercase().as_str() {
         "easy" => 1,
@@ -16,9 +16,9 @@ pub fn run_random_command(matches: &ArgMatches) {
             num.parse::<u32>().unwrap(),
         _ => 0,
     });
-
     start_solve(get_one_random_problem(difficulty_num));
 }
+
 pub fn run_solve_command(matches: &ArgMatches) {
     let problem_id = match matches.value_of("INPUT").unwrap().parse::<u32>() {
         Ok(id) => id,
